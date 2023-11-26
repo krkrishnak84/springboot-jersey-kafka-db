3482e91bd3fe339adc5442dd2c85893aec226303
Radha-K <43085235+krkrishnak84@users.noreply.github.com>
2023-11-25 23:06:50 -0500

    added files

--- src/main/java/org/radhatechi/spring/controller/TestController.java
+++ src/main/java/org/radhatechi/spring/controller/TestController.java
@@ -4,10 +4,14 @@ package org.radhatechi.spring.controller;
 import org.radhatechi.spring.database.CustomerDatabase;
 import org.radhatechi.spring.dto.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class TestController {
@@ -19,7 +23,34 @@ public class TestController {
 
     @GetMapping("/customers")
     public List<Customer> getAllCustomers(){
-       return CustomerDatabase.getAllCustomers();
+
+        //CustomerDatabase.getAllCustomers().stream().sorted((o1,o2) -> o1.getCustomerName().compareTo(o2.getCustomerName()))
+        //               .collect(Collectors.toList())
+
+        // sort by customer id
+        //CustomerDatabase.getAllCustomers().stream().sorted(Comparator.comparing(Customer::getCustomerId))
+        //.collect(Collectors.toList())
+
+        // sort by customer name
+       return CustomerDatabase.getAllCustomers().stream().sorted(Comparator.comparing(Customer::getCustomerName))
+               .collect(Collectors.toList());
+
     }
+
+    @GetMapping("/customers/group")
+    public Map<Long, List<Customer>> groupByCustomers(){
+        return CustomerDatabase.getAllCustomers().stream()
+                .collect(Collectors.groupingBy(Customer::getCustomerId));
+    }
+
+    @GetMapping("customers/{customerId}")
+    public List<Customer> getCustomer(@PathVariable long customerId){
+
+        return CustomerDatabase.getAllCustomers().stream()
+                .filter(customer -> customerId == customer.getCustomerId())
+                .collect(Collectors.toList());
+    }
+
+
 
 }
